c4c49ef53e2e38bfda0867c05fc96132834842e1
jeremypasimio <Jeremypasimio@gmail.com>
2017-12-02 20:27:43 -0800

    Create AllTests.java

---
+++ Checkers/src/Test/AllTests.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CheckerMoveTest.class, CheckersTest.class })
+public class AllTests {
+
+}
